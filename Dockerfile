7f1a76d5631806dc25a1adec3f8a405431c0b2dd
Gusionling <isdnhk@naver.com>
2025-04-03 00:22:16 +0900

    [add] : dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM bellsoft/liberica-openjdk-alpine:17
+ARG JAR_FILE=build/libs/*.jar
+ARG PROFILES
+ARG ENV
+# JAR_FILE 변수에 지정된 파일을 app.jar이름으로 컨테이너에 추가
+COPY ${JAR_FILE} app.jar
+#컨테이너가 실행될 때 기본적으로 실행될 명령어를 설정 , 이 경우에는 java 애플리케이션을 실행하는 명령어
+ENTRYPOINT ["java","-Dspring.profiles.active=${PROFILES}","-Dserver.env=${ENV}","-jar", "app.jar"]
--- src/main/java/com/hyeongkyu/template/controller/HealthCheckController.java
+++ src/main/java/com/hyeongkyu/template/controller/HealthCheckController.java
@@ -1,8 +1,11 @@
 package com.hyeongkyu.template.controller;
 
 import com.hyeongkyu.template.global.common.ResponseDto;
+import com.hyeongkyu.template.global.constants.Constants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,11 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping(Constants.API_PREFIX+"/health")
 public class HealthCheckController {
 
-    @GetMapping("/health")
+    @Value("${server.address}")
+    private String address;
+
+    @Value("${spring.profiles.active}")
+    private String env;
+
+    @GetMapping("/env")
     public ResponseDto<?> healthCheck() {
-        return ResponseDto.ok("health check");
+        return ResponseDto.ok(env);
     }
 
+    @GetMapping("/address")
+    public ResponseDto<?> getServerAddress(){
+        return ResponseDto.ok("서버 주소: "+address);
+    }
 }
